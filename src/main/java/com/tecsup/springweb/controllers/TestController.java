ac1de04b813fb2b8b8239d57dab8088e4cc92ee0
SABADO-MANHANA <SABADO-MANHANA@LAB707-06>
2014-09-06 14:01:20 -0500

    4

--- src/main/java/com/tecsup/springweb/controllers/TestController.java
+++ src/main/java/com/tecsup/springweb/controllers/TestController.java
@@ -4,12 +4,15 @@
  */
 package com.tecsup.springweb.controllers;
 
+import com.tecsup.springweb.model.Programa;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -21,21 +24,36 @@ import org.springframework.web.servlet.ModelAndView;
 //@RequestMapping("/")
 public class TestController {
 
+    @ModelAttribute
+    public void listado(Model model) {
+        model.addAttribute("nombres", new String[]{"jose", "jorge"});
+    }
+
+    @RequestMapping("parametros")
+    public String parametros(@RequestParam(required = false, defaultValue = "") String nombres,String apellidos, Model model) {
+        model.addAttribute("nombresCompleto", nombres + " " + apellidos);
+        return "test/informacion";
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("titulo", "Hola TECSUP");
         model.addAttribute("contenido", "Bienvenidos al curso Spring Framework");
+
+        Programa programa = new Programa();
+        programa.setDescripcion("Programa Prueba");
+        model.addAttribute("programa", programa);
         return "test/index";
     }
-/**
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView index() {
-        Map<String, Object> retorno = new HashMap<String, Object>();
-        retorno.put("titulo", "Hola TECSUP");
-        retorno.put("contenido", "Bienvenidos al curso Spring Framework");
-        return new ModelAndView("test/index", retorno);
-    }
-**/
+
+    /**
+     * @RequestMapping(method = RequestMethod.GET) public ModelAndView index() {
+     * Map<String, Object> retorno = new HashMap<String, Object>();
+     * retorno.put("titulo", "Hola TECSUP"); retorno.put("contenido",
+     * "Bienvenidos al curso Spring Framework"); return new
+     * ModelAndView("test/index", retorno); }
+*
+     */
     @RequestMapping("informacion")
     public String alternativo() {
         return "test/informacion";
--- src/main/webapp/WEB-INF/app/test/informacion.jsp
+++ src/main/webapp/WEB-INF/app/test/informacion.jsp
@@ -12,6 +12,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Informacion</h1>
+        <h1>Informacion: ${nombresCompleto} </h1>
     </body>
 </html>
